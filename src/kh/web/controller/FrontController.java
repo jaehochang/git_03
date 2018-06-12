7f3ba1c434b742096e47ac22e2bb02d6be24f637
Unknown <jang6-1@hanmail.net>
2018-06-12 15:57:33 +0900

    FrontController 수정

--- src/kh/web/controller/FrontController.java
+++ src/kh/web/controller/FrontController.java
@@ -1,40 +1,75 @@
 package kh.web.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FrontController
- */
-@WebServlet("/FrontController")
+@WebServlet("*.do")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public FrontController() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+		String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+
+		System.out.println("RequestURI : " + requestURI);
+		System.out.println("ContextPath : " + contextPath);
+
+		String command = requestURI.substring(contextPath.length()); // output.bo를 얻어옴
+
+		System.out.println(command); // 실제 커맨드를 얻어옴
+
+		MessagesDAO dao = new MessagesDAO();
+		boolean isRedirect = true;
+		String dst = null; 
+		if (command.equals("/output.do")) {
+			List<MessagesDTO> result = dao.getAllData();
+			request.setAttribute("result", result);
+			isRedirect = false;
+			dst = "outputView.jsp";
+			
+		} else if (command.equals("/input.do")) {
+			String name = request.getParameter("name");
+			String msg = request.getParameter("msg");
+			System.out.println(name);
+			System.out.println(msg);
+			int result = dao.insertInfo(name, msg);
+			request.setAttribute("result", result);
+			isRedirect = false;
+			dst = "inputView.jsp";
+		}
+
+		if (isRedirect == false) {
+			RequestDispatcher rd = request.getRequestDispatcher(dst);
+			rd.forward(request, response);
+		} else {
+			response.sendRedirect(dst);
+		}
+		// redirect를 해야할때 request를 하면 참사가 난다.
+
+	} catch(Exception e) {
+		e.printStackTrace();
+		response.sendRedirect("error.html");
+	}
+	}
+	
+
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 	}
 
